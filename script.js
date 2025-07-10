80ada5022afa7610bca9da06ad1d5cdac81d2587
İbrahim Akay <>
2025-07-10 22:31:27 +0300

    Revert "header issue 16"
    This reverts commit 59186be12fe863f0972767099135d0728a4f079b.

--- script.js
+++ script.js
@@ -100,13 +100,8 @@ const cities = [
 
 document.addEventListener('DOMContentLoaded', function () {
     const map = L.map('map', {
-        tap: false,
-        touchZoom: false,
-        scrollWheelZoom: false,
-        doubleClickZoom: false,
-        zoomControl: true,
-        dragging: true,
-        closePopupOnClick: false,
+        tap: false, // Bu satırı ekleyin
+        closePopupOnClick: false // Bu satırı ekleyin
     });
 
     L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {

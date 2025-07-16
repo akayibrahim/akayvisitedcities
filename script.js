843f64c033b58e7b03eb62264cfabee42f3fee07
İbrahim Akay <>
2025-07-16 17:01:29 +0300

    header issue 32

--- script.js
+++ script.js
@@ -99,10 +99,7 @@ const cities = [
 ];
 
 document.addEventListener('DOMContentLoaded', function () {
-    const map = L.map('map', {
-        tap: false, // Bu satırı ekleyin
-        closePopupOnClick: false // Bu satırı ekleyin
-    });
+    const map = L.map('map');
 
     L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
